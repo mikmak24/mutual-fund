051ecf7015dbcc388f7c964d5621b04183be57ff
mpanabe0124 <m.panabe@decodetech.ph>
2022-09-19 15:25:09 +0800

    Fixed

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'password', 'employee_monthly_contribution', 'is_admin'
+        'username', 'password', 'employee_monthly_contribution', 'is_admin', 'is_active'
     ];
 
     /**
