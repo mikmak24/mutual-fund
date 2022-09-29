f5f856d9f54e534dccd53c201b8756b285e1eeff
mpanabe0124 <m.panabe@decodetech.ph>
2022-09-29 14:38:56 +0800

    Fixed

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -83,10 +83,8 @@ const router = new VueRouter({
 
 })
 
-// if (store.getters['authentication/isAuthenticated'] === false) {
-//     router.replace('login')
-// } else {
-//     router.replace('dashboard')
-// }
+if (store.getters['authentication/isAuthenticated'] === false) {
+    router.replace('login')
+}
   
 export default router
